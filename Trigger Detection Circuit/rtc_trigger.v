9aa75fd5e757e1721285cb25217623b7596475ef
eric-wagne <eric.wagner@ltts.com>
2020-06-19 09:59:13 -0500

    eric-trigger-syn corrections
    adjusted sensitivity list for systhesis

--- Trigger Detection Circuit/rtc_trigger.v
+++ Trigger Detection Circuit/rtc_trigger.v
@@ -7,12 +7,13 @@
 //
 module rtc_trigger(
 //--------------------------------Port Declrations--------------------------------
-    input wire i_sclk,          //System clock
-    input wire i_reset_n,       //Active low asyn reset
-    input wire i_trigger,       //Push button input
-    output reg o_count_init,    //Output control signal
-    output reg o_count_enb,     //Output control signal
-    output reg o_latch_count    //Output control signal
+    input  wire i_sclk,          //System clock
+    input  wire i_reset_n,       //Active low asyn reset
+    input  wire i_trigger,       //Push button input
+    
+    output wire o_count_init,    //Output control signal
+    output wire o_count_enb,     //Output control signal
+    output wire o_latch_count    //Output control signal
     );
 //--------------------------------------------------------------------------------
     parameter BOUND = 1000000;           //Used to set the Debounce time
@@ -22,6 +23,10 @@ module rtc_trigger(
     parameter [1:0] PAUSE    = 2'b10;    //
      
 //--------------------------------Internal Signals--------------------------------
+    reg r_count_init;
+    reg r_count_enb;
+    reg r_latch_count;
+    
     reg [1:0] curState_t;   //Current State (IDLE, COUNTING, or PAUSE)
     reg [1:0] nxtState_t;   //Next State (IDLE, COUNTING, or PAUSE)
     
@@ -31,9 +36,11 @@ module rtc_trigger(
 //--------------------------------------------------------------------------------
 
 //--------------------------------Next State Logic--------------------------------
-    always @ (posedge triggerDB)
+    always @ (posedge triggerDB, negedge i_reset_n)
     begin
-        if (curState_t == IDLE) begin
+        if (i_reset_n == 0) begin
+            nxtState_t = IDLE;
+        end else if (curState_t == IDLE) begin
             nxtState_t = COUNTING;
         end else if (curState_t == COUNTING) begin
             nxtState_t = PAUSE;
@@ -50,30 +57,33 @@ module rtc_trigger(
     begin
         if (i_reset_n == 0) begin
             curState_t = IDLE;
-            nxtState_t = IDLE;
         end
         curState_t = nxtState_t;
         case(curState_t)
             IDLE : begin
-                o_count_init = 1;
-                o_count_enb = 0;
-                o_latch_count = 0;
+                r_count_init  = 1;
+                r_count_enb   = 0;
+                r_latch_count = 0;
             end
             COUNTING : begin
-                o_count_init = 0;
-                o_count_enb = 1;
-                o_latch_count = 1;
+                r_count_init  = 0;
+                r_count_enb   = 1;
+                r_latch_count = 1;
             end
             PAUSE : begin
-                o_count_init = 0;
-                o_count_enb = 1;
-                o_latch_count = 0;
+                r_count_init  = 0;
+                r_count_enb   = 1;
+                r_latch_count = 0;
             end
             default : begin
                 curState_t = IDLE;
             end
         endcase
     end
+    
+    assign o_count_init  = r_count_init;
+    assign o_count_enb   = r_count_enb;
+    assign o_latch_count = r_latch_count;
 //--------------------------------------------------------------------------------
 
 //---------------------------------Debounce Logic---------------------------------
@@ -82,7 +92,7 @@ begin
     if(i_reset_n == 0) begin
         countDB = 0;
         triggerDB = 0;
-        prvTrigger = 1'b0;
+        prvTrigger = 0;
     end else begin
         if (countDB == 0) begin
             prvTrigger = i_trigger;
